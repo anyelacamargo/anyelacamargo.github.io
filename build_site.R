46fbf66fa3aef4f6de259fa97de396f97b9a454e
anyelacamargo <anyelavcamargo@gmail.com>
2018-03-10 18:14:48 +0000

    test4

--- build_site.R
+++ build_site.R
@@ -2,7 +2,8 @@
 #This helps avoid confusion if our working directory is 
 #not our site because of other projects we were 
 #working on at the time. 
-#setwd("/Users/Nick/personal_site")
+setwd("D:/Anyela/repo/anyelacamargo.github.io")
 
 #render your sweet site. 
 rmarkdown::render_site()
+
